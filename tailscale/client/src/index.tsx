93aa99170c8a4248d55dd3b13604366fda6c657f
Ross Zurowski <ross@rosszurowski.com>
2022-01-04 11:23:34 -0500

    Update Tailscale dependencies, clean up config, fix linter errors
    This commit is some initial setup on the Tailscale extension folder
    before we start work on the extension itself.

--- tailscale/client/src/index.tsx
+++ tailscale/client/src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import { App } from "./app"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+  document.getElementById("root"),
+)
